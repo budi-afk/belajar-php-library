4b2b1a792a344ef746c4bfe5d0d9802a6502774b
budi-afk <budiman1347@gmail.com>
2022-10-30 18:22:24 +0700

    Add default name value

--- src/Customer.php
+++ src/Customer.php
@@ -1,10 +1,15 @@
-<?php 
+<?php
+
 namespace Budiman\BelajarPhpLibrary;
 
-class Customer {
-    public function __construct(private string $name) {}
+class Customer
+{
+    public function __construct(private string $name)
+    {
+    }
 
-    public function sayHello(string $name): string {
+    public function sayHello(string $name = "Guest"): string
+    {
         return "Hello $name, nama saya $this->name";
     }
 }
